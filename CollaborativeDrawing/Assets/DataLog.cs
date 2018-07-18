eee628aea80a9380f0d403db986fe825204fd87c
DataVis9 <40277400+DataVis9@users.noreply.github.com>
2018-07-18 14:01:44 -0400

    First commit

---
+++ CollaborativeDrawing/Assets/DataLog.cs
@@ -0,0 +1,241 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+//using Vuforia;
+using System.IO;
+
+public class DataLog : MonoBehaviour {
+
+    public GameObject RedBrush;
+    public GameObject GreenBrush;
+    public GameObject BlueBrush;
+    public GameObject GradBrush;
+
+    public List<Vector3> RtransformLocations; 
+    public List<Vector3> GtransformLocations;
+    public List<Vector3> BtransformLocations;
+    public List<Vector3> GradtransformLocations;
+
+    public List<string> colorOrder;
+
+    private Vector3 RLastLoc;
+    private Vector3 GLastLoc;
+    private Vector3 BLastLoc;
+    private Vector3 GradLastLoc;
+
+    private int cntr;
+
+
+    private Ray ray = new Ray();
+ 
+    private string path;
+    private TextWriter writer;
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+
+
+        //if Data Log has no game object for brush, uncomment the lines below ...
+
+        // RedBrush = GameObject.Find("RedBrush");
+        // GreenBrush = GameObject.Find("GreenBrush");
+        // BlueBrush = GameObject.Find("BlueBrush");
+        // GradBrush = GameObject.Find("GradientBrush");
+
+        cntr = 0;
+    
+        RtransformLocations = new List<Vector3>();
+        RLastLoc = RedBrush.transform.position;
+        GLastLoc = GreenBrush.transform.position;
+        BLastLoc = BlueBrush.transform.position;
+        GradLastLoc = GradBrush.transform.position;
+
+
+        
+
+        path = Path.Combine(Application.persistentDataPath, "LogFile.txt");
+        writer = File.CreateText(path);
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ray.direction = Camera.main.transform.forward;
+        ray.origin = Camera.main.transform.position;
+        /*  RaycastHit hitInfo;
+          if (Physics.Raycast(
+                  Camera.main.transform.position,
+                  Camera.main.transform.forward,
+                  out hitInfo,
+                  20.0f,
+                  Physics.DefaultRaycastLayers))
+          {
+              // If the Raycast has succeeded and hit a hologram
+              // hitInfo's point represents the position being gazed at
+              //Debug.Log("GREAT SCOTT " + hitInfo.transform.position);
+              // hitInfo's collider GameObject represents the hologram being gazed at
+          }*/
+
+
+
+
+
+        //if cntr is divisible by 59; aka, every 59 frames, do this hullaballoo underneath
+        if (cntr % 59 == 0)
+        {
+
+            Debug.Log(Time.time);
+            writer.Write("Time: " + Time.time);
+
+            //    Debug.Log(RaycastHit);
+
+            Debug.Log("GREAT SCOTT! " + ray.direction);
+
+
+
+            //write data about the Red brush's location to file
+            if (RLastLoc != RedBrush.transform.position)
+            {
+                Debug.Log(" - Red Brush Position: " + RedBrush.transform.position);
+                writer.Write(" - Red Brush Position: " + RedBrush.transform.position);
+                RtransformLocations.Add(RedBrush.transform.position);
+                RLastLoc = RedBrush.transform.position;
+            
+                //  Debug.Log("Total red position points collected: " + RtransformLocations.Count);
+                //  RTime = RTime + (Time.deltaTime /* - RTime*/);
+                // Debug.Log("Time: " + RTime);
+
+                // colorOrder.Add("Red at " + Time.time);
+
+
+            }
+
+            //write data about the Green brush's location to file
+            if (GLastLoc != GreenBrush.transform.position)
+            {
+                Debug.Log(" - Green Brush Position: " + GreenBrush.transform.position);
+                writer.Write(" - Green Brush Position: " + GreenBrush.transform.position);
+                GtransformLocations.Add(GreenBrush.transform.position);
+                GLastLoc = GreenBrush.transform.position;
+               
+                //   Debug.Log("Total green position points collected: " + GtransformLocations.Count);
+                // GTime = GTime + (Time.deltaTime /*- GTime*/);
+                //  Debug.Log("Time: " + GTime);
+             //   colorOrder.Add("Green at " + Time.time);
+                // Instantiate
+
+            }
+
+            //write data about the Blue brush's location to file
+            if (BLastLoc != BlueBrush.transform.position)
+            {
+                Debug.Log(" - Blue Brush Position: " + BlueBrush.transform.position);
+                writer.Write("Blue Brush Position: " + BlueBrush.transform.position);
+                BtransformLocations.Add(BlueBrush.transform.position);
+                BLastLoc = BlueBrush.transform.position;
+               
+                //  Debug.Log("Total blue position points collected: " + BtransformLocations.Count);
+                // BTime = BTime + Time.deltaTime;
+               // colorOrder.Add("Blue at " + Time.time);
+
+            }
+
+            //write data about the Gradient brush's location to file
+            if (GradLastLoc != GradBrush.transform.position)
+            {
+                Debug.Log(" - Gradient Brush Position: " + GradBrush.transform.position);
+                writer.WriteLine("Gradient Brush Position: " + GradBrush.transform.position);
+                GradtransformLocations.Add(GradBrush.transform.position);
+                GradLastLoc = GradBrush.transform.position;
+               
+                // Debug.Log("Total gradient position points collected: " + GradtransformLocations.Count);
+
+               // colorOrder.Add("Gradient at " + Time.time);
+
+            }
+        }
+        cntr++;  
+    }
+
+/*
+    public void OnApplicationQuit()
+    {
+        //red
+        Debug.Log("Red Brush's Positions: ");
+        for(int i = 0; i<RtransformLocations.Count; i++)
+        {
+            Debug.Log(RtransformLocations.ElementAt(i));
+        }
+        writer.WriteLine("Red Brush's Positions: ");
+        for (int i = 0; i < RtransformLocations.Count; i++)
+        {
+            writer.WriteLine(RtransformLocations.ElementAt(i));
+        }
+
+        //Green
+        Debug.Log("Green Brush's Positions: ");
+        for (int i = 0; i < GtransformLocations.Count; i++)
+        {
+            Debug.Log(GtransformLocations.ElementAt(i));
+        }
+        writer.WriteLine("Green Brush's Positions: ");
+        for (int i = 0; i < GtransformLocations.Count; i++)
+        {
+            writer.WriteLine(GtransformLocations.ElementAt(i));
+        }
+
+        //Blue
+        Debug.Log("Blue Brush's Positions: ");
+        for (int i = 0; i < BtransformLocations.Count; i++)
+        {
+            Debug.Log(BtransformLocations.ElementAt(i));
+        }
+        writer.WriteLine("Blue Brush's Positions: ");
+        for (int i = 0; i < BtransformLocations.Count; i++)
+        {
+            writer.WriteLine(BtransformLocations.ElementAt(i));
+        }
+
+        //Gradient
+        Debug.Log("Gradient Brush's Positions: ");
+        for (int i = 0; i < GradtransformLocations.Count; i++)
+        {
+            Debug.Log(GradtransformLocations.ElementAt(i));
+        }
+        writer.WriteLine("Gradient Brush's Positions: ");
+        for (int i = 0; i < GradtransformLocations.Count; i++)
+        {
+            writer.WriteLine(GradtransformLocations.ElementAt(i));
+        }
+
+        //Color order and timing
+        Debug.Log("color order: ");
+        for (int i = 0; i < colorOrder.Count; i++)
+        {
+            Debug.Log(colorOrder.ElementAt(i));
+        }
+        writer.WriteLine("color order: ");
+        for (int i = 0; i < colorOrder.Count; i++)
+        {
+            writer.WriteLine(colorOrder.ElementAt(i));
+        }
+    }
+    */
+
+    public void collabDesired()
+    {
+        //this method always causes an error in unity; best idea would be to just manually track whether or not the test used collaboration or not
+        Debug.Log("test");
+        writer.WriteLine("Collaboration desired");
+    }
+
+
+}
+
---
+++ CollaborativeDrawing/Assets/SpeechCommandHandler.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkitExtensions.Messaging;
+
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+using System;
+//using HoloPixelArt.Messages;
+
+namespace CollaborativeDrawing
+{
+    public class SpeechCommandHandler : MonoBehaviour //, ISpeechHandler
+    {
+
+        public string[] keywords = new string[] { "red", "green", "blue" };
+        public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+
+        protected PhraseRecognizer recognizer;
+        protected string word = "right";
+
+        ParticleSystem partBlue;
+        ParticleSystem partGreen;
+
+        public GameObject bubbles;
+        int count;
+        int cntrR;
+        int cntrG;
+       
+
+        public GameObject RedBrush;
+        public GameObject GreenBrush;
+        public GameObject BlueBrush;
+     
+
+        public List<Vector3> RtransformLocations;
+        public List<Vector3> GtransformLocations;
+        public List<Vector3> BtransformLocations;
+  
+
+        public Quaternion rquat;
+
+        private Vector3 RLastLoc;
+        private Vector3 GLastLoc;
+       // private Vector3 BLastLoc;
+
+
+
+        void Start()
+        {
+
+            if (keywords != null)
+            {
+                recognizer = new KeywordRecognizer(keywords, confidence);
+                recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+                recognizer.Start();
+
+            }
+
+
+            partBlue = BlueBrush.GetComponent<ParticleSystem>();
+            partGreen = GreenBrush.GetComponent<ParticleSystem>();
+
+            partBlue.Stop();
+            partGreen.Stop();
+
+            count = 0;
+            RtransformLocations = new List<Vector3>();
+            RLastLoc = RedBrush.transform.position;
+            GLastLoc = GreenBrush.transform.position;
+         //   BLastLoc = BlueBrush.transform.position;
+           
+            rquat = RedBrush.transform.rotation;
+  
+            
+        }
+
+        private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+        {
+            word = args.text;
+          //  results.text = "You said: <b>" + word + "</b>";
+        }
+
+        public void blueCollab()
+        {
+           
+
+            count++;
+            if (count % 2 != 0)
+            {
+                partBlue.Play();
+            } else
+            {
+                partBlue.Stop();
+            }
+        }
+
+        public void redCollab()
+        {
+            cntrR++;
+            if(cntrR%2 != 0)
+            {
+                for (int i = 0; i < RtransformLocations.Count; i=i+2)
+                  {
+                      Instantiate(bubbles, 2 * RtransformLocations[i], rquat);
+
+                   }
+            } else
+            {
+
+            }
+            
+        }
+
+        public void greenCollab()
+        {
+            cntrG++;
+            if (cntrG % 2 != 0)
+            {
+                partGreen.Play();
+
+            } else
+            {
+                partGreen.Stop();
+            }
+        }
+
+
+        //turn on the renderer for "CollbObjects..."
+        public void Update()
+        {
+            switch (word)
+            {
+                case "blue":
+                    blueCollab();
+                    break;
+                case "green":
+                    greenCollab();
+                    break;
+                case "red":
+                    redCollab();
+                    break;
+            }
+        }
+
+
+        /* public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+         {
+           /*  count++;
+             if(count%2 != 0) //if count = 1, 3, 5...
+             {
+
+                     part.Play();
+                     partG.Play();
+
+
+                 sting = true;
+             } else
+             {
+                 sting = false;
+                 part.Stop();
+                 partG.Stop();
+             }
+
+            // collabs.SetActive(true);
+
+           //  DataLog vis = new DataLog();
+           //  vis.collabDesired();
+
+
+
+          //   var ins = new DataLog();
+          //   ins.collabDesired();
+            // throw new NotImplementedException();
+         }*/
+
+        private void OnApplicationQuit()
+        {
+            if (recognizer != null && recognizer.IsRunning)
+            {
+                recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+                recognizer.Stop();
+            }
+        }
+
+
+    }
+}
+
